0d299d2bd5344cfa2b7c10b4633180b89c843d4c
Awat Singdamrong <110981996+pinkyPingy@users.noreply.github.com>
2023-04-26 13:41:37 +0700

    fix: change db to todolist_db, feat: itemController.addItem, deleteItem

--- controller/itemsController.js
+++ controller/itemsController.js
@@ -42,11 +42,22 @@ exports.getItems = async (req, res) => {
 // TODO #1.2: Add an item to DynamoDB
 exports.addItem = async (req, res) => {
   const item_id = uuidv4();
-  const created_date = Date.now();
-  const item = { item_id: item_id, ...req.body, created_date: created_date };
+  // const created_date = Date.now();
+  const item = { item_id: item_id, ...req.body };
 
   // You should change the response below.
-  res.send("This route should add an item in DynamoDB.");
+  // res.send("This route should add an item in DynamoDB.");
+  const params = {
+    TableName: process.env.aws_items_table_name,
+    Item: item
+  };
+  try {
+    const data = await docClient.send(new PutCommand(params));
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
 
 // TODO #1.3: Delete an item from DynamDB
@@ -54,5 +65,18 @@ exports.deleteItem = async (req, res) => {
   const item_id = req.params.item_id;
 
   // You should change the response below.
-  res.send("This route should delete an item in DynamoDB with item_id.");
+  // res.send("This route should delete an item in DynamoDB with item_id.");
+  const params = {
+    TableName: process.env.aws_items_table_name,
+    Key: {
+      item_id: item_id
+    }
+  };
+  try {
+    const data = await docClient.send(new DeleteCommand(params));
+    res.send(data);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err);
+  }
 };
